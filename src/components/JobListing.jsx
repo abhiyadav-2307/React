5fd11425b5ab22f26cada0e18b2d93c4219ab4cb
Abhishek Yadav <abhi1611919@gmail.com>
2024-12-17 23:03:48 +0530

    show more less and icon added

--- src/components/JobListing.jsx
+++ src/components/JobListing.jsx
@@ -1,9 +1,16 @@
 import React from 'react'
+import {FaMapMarker} from 'react-icons/fa'
 
+import { useState } from 'react'
 
 const JobListing = ({job}) => {
- 
-    return (
+ const [showFullDescriptions, setShowFullDescription] = useState(false);
+    
+ let description = job.description;
+ if(!showFullDescriptions){
+  description=description.substring(0,100)+ '...';
+ }
+ return (
     <div
               className="bg-white rounded-xl shadow-md relative"
               key={job.id}
@@ -15,8 +22,12 @@ const JobListing = ({job}) => {
                 </div>
 
                 <div className="mb-5">
-                  {job.description}
+                  {description}
                 </div>
+
+               <button onClick={ ()=>setShowFullDescription((prevState)=>!prevState) } className="text-indigo-500 mb-5 hover:text-indigo-600"  >
+                {showFullDescriptions ? 'Less':'More'}
+               </button>
 
                 <h3 className="text-indigo-500 mb-2">{job.salary} / Year</h3>
 
@@ -24,7 +35,7 @@ const JobListing = ({job}) => {
 
                 <div className="flex flex-col lg:flex-row justify-between mb-4">
                   <div className="text-orange-700 mb-3">
-                    <i className="fa-solid fa-location-dot text-lg"></i>
+                    <FaMapMarker className='inline text-lg mb-1'/>
                     {job.location}
                   </div>
                   <a
