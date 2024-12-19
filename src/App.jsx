eae208c17cf17829f0f03319d459f869542eb48e
Abhishek Yadav <abhi1611919@gmail.com>
2024-12-19 16:01:50 +0530

    Add new job with form funtionalaty added But post functionality is not added yet

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,36 @@
-import {Route, createBrowserRouter, createRoutesFromElements,RouterProvider} from 'react-router-dom'
+import {
+  Route,
+  createBrowserRouter,
+  createRoutesFromElements,
+  RouterProvider,
+} from "react-router-dom";
 import React from "react";
-import HomePage from './pages/HomePage';
-import JobsPage from './pages/JobsPage';
-import MainLayout from './layouts/MainLayout';
-import NotFoundPage from './pages/NotFoundPage';
-import JobPage, {jobLoader} from './pages/JobPage';
-
-const router = createBrowserRouter(
-  createRoutesFromElements(
-  <Route path='/' element = {<MainLayout/>} >
-    <Route index element = { <HomePage/> } />
-    <Route path='/jobs' element = { <JobsPage/> } />
-    <Route path='/jobs/:id' element = { <JobPage/> } loader = {jobLoader} />
-    <Route path='*' element = { <NotFoundPage/> } />
-  </Route>
-  
-)
-);
+import HomePage from "./pages/HomePage";
+import JobsPage from "./pages/JobsPage";
+import MainLayout from "./layouts/MainLayout";
+import NotFoundPage from "./pages/NotFoundPage";
+import JobPage, { jobLoader } from "./pages/JobPage";
+import AddJobPage from "./pages/AddJobPage";
 
 const App = () => {
-  return <RouterProvider router = {router} />;
+  
+  const addJob = (newJob) => {
+    console.log(newJob);
+  };
+
+  const router = createBrowserRouter(
+    createRoutesFromElements(
+      <Route path="/" element={<MainLayout />}>
+        <Route index element={<HomePage />} />
+        <Route path="/jobs" element={<JobsPage />} />
+        <Route path="/add-job" element={<AddJobPage addJobSubmit={addJob} />}  />
+        <Route path="/jobs/:id" element={<JobPage />} loader={jobLoader} />
+        <Route path="*" element={<NotFoundPage />} />
+      </Route>
+    )
+  );
+
+  return <RouterProvider router={router} />;
 };
 
 export default App;
